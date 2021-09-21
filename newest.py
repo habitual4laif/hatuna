f1c6aa5a3d1a2942d99f557d68e0f810fa63469d
Abiodun Agbolade <biodun.agbolade@gmail.com>
2021-09-21 14:21:42 +0100

    Py if/else statement

--- newest.py
+++ newest.py
@@ -3,3 +3,20 @@ dat = "we are here already. "
 p = 3
 bee = dat * p
 print(bee)
+
+#
+players = [34, 64, 74, 92, 16, 73]
+players[:2] #we need the first two
+
+#
+players = [34, 64, 74, 92, 16, 73]
+players[2:] #we need the numbers after the first two
+
+#if/else statement
+name = 19
+if name < 18:
+    print("Drink is not for you")
+elif name == 18:
+    print("You have to have celebrated your 18th")
+else:
+    print("Enjoy your drink")

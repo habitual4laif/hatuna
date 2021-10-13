1bffd295717a8d3c172e4092419beb79cbacd99b
Abiodun Agbolade <biodun.agbolade@gmail.com>
2021-10-13 18:05:23 +0100

    web crawler

--- newest.py
+++ newest.py
@@ -217,3 +217,38 @@ def download_csv(csv_url):
     file_name.close()
 
 download_csv(csv_url)
+
+#Web crawlers Videos 25-27
+import requests
+from bs4 import BeautifulSoup
+
+def trade_spider(max_pages):
+    page = 0
+    while page <= max_pages:
+        url = 'https://www.hotnigerianjobs.com/field/223/' + str(page)
+        source_code = requests.get(url)
+        plain_text = source_code.text
+        soup = BeautifulSoup(plain_text, 'html5lib')
+        for link in soup.findAll('a', {'rel': 'bookmark'}):
+            href = link.get('href')
+            title = link.string
+           # print(href)
+            #print(title)
+            get_single_item_data(href)
+
+        page += 1
+
+
+def get_single_item_data(item_url):
+    source_code = requests.get(item_url)
+    plain_text = source_code.text
+    soup = BeautifulSoup(plain_text, 'html5lib')
+    for item_name in soup.findAll('a', {'rel': 'bookmark'}):
+        print(item_name.string)
+    for link in soup.findAll('a'):
+        href = link.get('href')
+        print(href)
+
+trade_spider(1)
+
+

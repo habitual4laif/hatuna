b9284ef4e86cd3d9e7d3b5a2c654c80b8139ff67
Abiodun Agbolade <biodun.agbolade@gmail.com>
2021-10-24 17:44:13 +0100

    Word counter

--- newest.py
+++ newest.py
@@ -368,3 +368,43 @@ send = Messager(name = 'We are sending a Message')
 receive = Messager(name = 'We are receiveing a Message')
 send.start()
 receive.start()
+
+
+#Word frequency counter
+import requests
+from bs4 import BeautifulSoup #Whenever we want to pick things from the webpage we need BeautifulSoup
+import operator
+
+def bstart(url):
+    word_list = []
+    source_code = requests.get(url).text #the dot text (.text) makes it return text and not other form of dataset
+    bsoup = BeautifulSoup(source_code, 'html5lib') #whenever we use BeautifulSoup, we will need a soup object (name it anything)
+    for post_text in bsoup.findAll('a', {'rel': 'bookmark'}): #find all a tags that has this class attributes
+        content = post_text.string #dot string (.string) will only take the content in the html tag
+        words = content.lower().split() #dot lower (.lower) makes all words in lower case while .split break each word
+        for each_word in words:
+            word_list.append(each_word)
+    clean_up_list(word_list)
+
+def clean_up_list(word_list):
+    clean_word_list = []
+    for word in word_list:
+        symbols = "!@#$%^&*)(_-+=~<>?:|}{\"[];'\/.,"
+        for i in range(0, len(symbols)):
+            word = word.replace(symbols[i], "")
+        if len(word) > 0:
+            clean_word_list.append(word)
+    create_dictionary(clean_word_list)
+
+
+def create_dictionary(clean_word_list):
+    word_count = {}
+    for word in clean_word_list:
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+    for key, value in sorted(word_count.items(), key = operator.itemgetter(1)):
+        print(key, value)
+
+bstart('https://www.hotnigerianjobs.com/field/223/electrical-electronic-engineering-jobs-in-nigeria')
